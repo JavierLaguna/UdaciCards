823e86872d73822209cc05ee71b4b87b28872885
Javier Laguna <javier.laguna92@gmail.com>
2017-11-28 17:05:30 +0100

    TabNavigator styles

--- App.js
+++ App.js
@@ -1,38 +1,43 @@
 import React, {Component} from 'react';
-import {View} from 'react-native'
+import {View, Platform} from 'react-native'
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {TabNavigator} from 'react-navigation';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 import DeckListView from './src/containers/DeckListView';
 import reducer from './src/reducers';
+import {white, orange, blue} from './utils/colors';
 
 const Tabs = TabNavigator({
         DeckList: {
             screen: DeckListView,
             navigatonOptions: {
                 tabBarLabel: 'Deck List',
-                // tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+                tabBarIcon: ({tintColor}) => <MaterialCommunityIcons name='cards-outline' size={30} color={tintColor}/>
             }
         }
     },
     {
-        // navigatonOptions: {
-        //     header: null
-        // },
-        // tabBarOptions: {
-        //     activeTintColor: Platform.OS === 'ios' ? purple : white,
-        //     style: {
-        //         height: 56,
-        //         backgroundColor: Platform.OS === 'ios' ? white : purple,
-        //         shadowColor: 'rgba(0, 0, 0, 0.24)',
-        //         shadowOffset: {
-        //             width: 0,
-        //             height: 3
-        //         },
-        //         shadowRadius: 6,
-        //         shadowOpacity: 1
-        //     }
-        // }
+        navigatonOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: Platform.OS === 'ios' ? orange : white,
+            indicatorStyle: {
+                backgroundColor: blue
+            },
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === 'ios' ? white : orange,
+                shadowColor: 'rgba(0, 0, 0, 0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+            }
+        }
     });
 
 
