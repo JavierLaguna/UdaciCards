10a2f845a060c9dd8df370a407551492355d3ef8
Javier Laguna <javier.laguna92@gmail.com>
2017-12-05 16:14:21 +0100

    add NewDeckView container

---
+++ src/containers/NewDeckView.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class NewDeckView extends Component {
+
+    state = {
+
+    };
+
+    render() {
+        return (
+            <View>
+                <Text>A</Text>
+            </View>
+        )
+    }
+}
