37d2a39597b7def0dd7bf4eba3aadc08b75e5a2b
Javier Laguna <javier.laguna92@gmail.com>
2017-12-07 09:04:50 +0100

    input props

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react'
 import {StyleSheet, TextInput} from 'react-native'
 import {blue, gray} from "../../../utils/colors";
+import PropTypes from 'prop-types'
 
 export default class Input extends Component {
+    static propTypes = {
+        style: PropTypes.object
+    };
+
+    static defaultProps = {
+        style: {}
+    };
+
     render() {
+        const {style} = this.props;
+
         return (
             <TextInput {...this.props}
                        placeholderTextColor={gray}
                        selectionColor={blue}
                        underlineColorAndroid={blue}
-                       style={styles.input}
+                       style={[styles.input, style]}
             />
         )
     }
